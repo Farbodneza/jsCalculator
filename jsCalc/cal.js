b93f542b7efc7bc2b24c2efed0923ffe6bacfc88
Farbod Nezafati <farbodneza@gmail.com>
2024-09-05 20:01:37 +0330

    Java Script of Calculator

--- jsCalc/cal.js
+++ jsCalc/cal.js
@@ -1,1 +1,18 @@
-const display = document.getElementById('display')
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        'Error';
+    }
+}
